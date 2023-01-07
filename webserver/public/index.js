f33e407756849a365c637979274806703aa0958e
AbaSkillzz <aba@skillzz>
2023-01-07 14:50:28 +0100

    finished list inspirations visualization, started delete btn

--- webserver/public/index.js
+++ webserver/public/index.js
@@ -1,3 +1,4 @@
+//LIST INSPIRATION BUTTON
 document.getElementById("list-inspirations-btn").addEventListener("click", () => {
    const xhttp = new XMLHttpRequest();
 
@@ -13,19 +14,32 @@ document.getElementById("list-inspirations-btn").addEventListener("click", () =>
             //everithing inside each inspiration's div
             const h2 = document.createElement("h2");
             h2.innerText = response[i].name
+            h2.style.color = "gold";
             newDiv.appendChild(h2); // add inner things to single inspiration's div
 
-            //HEREEEEEEE idea = (add h3 to p )
+            let div = document.createElement("div");
             let h3 = document.createElement("h3");
+            h3.style.display = "inline";
             h3.innerText = "Description: ";
             let p = document.createElement("p");
+            p.style.display = "inline";
             p.innerText = response[i].description;
-            
-            newDiv.appendChild(h3);
 
+            div.appendChild(h3);
+            div.appendChild(p);
+            newDiv.appendChild(div);
+
+            div = document.createElement("div");
             h3 = document.createElement("h3");
-            h3.innerText = `Fields of influence: ${response[i].influenceField}`;
-            newDiv.appendChild(h3);
+            h3.style.display = "inline";
+            h3.innerText = "Fields of influence: ";
+            p = document.createElement("p");
+            p.style.display = "inline";
+            p.innerText = response[i].influenceField;
+
+            div.appendChild(h3);
+            div.appendChild(p);
+            newDiv.appendChild(div);
 
             //add new_div to list_div in html file
             listDiv.appendChild(newDiv);
@@ -38,3 +52,7 @@ document.getElementById("list-inspirations-btn").addEventListener("click", () =>
 });
 
 
+//DELETE INSPIRATION BUTTON
+document.getElementById("delete-inspiration-btn").addEventListener("click", () => {
+   
+});
